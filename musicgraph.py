1a66741c685d9d087d6a1a0515a1d7cbc3b7619f
alecksphillips <alecks.phillips@gmail.com>
2014-06-17 02:52:40 +0100

    Main bulk of the work done. Need to work on graph output now

--- musicgraph.py
+++ musicgraph.py
@@ -35,17 +35,143 @@ try:
     from urllib.request import urlopen
 except ImportError:
     from urllib2 import urlopen
+try:
+    from urllib.parse import urlquote as quote
+except ImportError:
+    from urllib import quote
 from xml.dom import minidom
 import os.path
 import sys
+import time
 from getopt import getopt
-from graphviz import Digraph
+import pydot
 
-def getData(username,number):
+def makeGraph(username,number,cache,Local):
     #Get users top artists
     
-    url="
+    # make cache if doesn't exist
+    if not os.path.exists(cache):
+        print("cache directory ("+cache+") doesn't exist. I'm creating it.")
+        os.mkdir(cache)
+        os.mkdir(cache+os.sep+'similar')
+        
+    url=('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user='+username+
+         '&api_key='+api_key+'&limit=20')
+        
+        
+    # Make a local copy of the charts
+    local_copy = cache+os.sep+'charts_'+username+'.xml'
+    if Local=='no' or (Local=='yes' and not os.path.isfile(local_copy)):
+        try:
+            print("Downloading from ",url)
+            download(url, local_copy)
+        except Exception as err:
+            print("#"*20)
+            print("I couldn't download the profile or make a local copy of it.")
+            print("#"*20)
+    else:
+        print("Reading from local copy:  ",local_copy)
+        
+    print("Parsing list of top artists...")
+    
+    try:
+        data=open(local_copy,'rb')
+        xmldoc=minidom.parse(data)
+        data.close()
+
+    except Exception as err:
+        print('#'*20)
+        print("Error while parsing your profile. Your username might be "
+              "misspelt or your charts empty.")
+        print('#'*20)
+        sys.exit()
+
+    artistlist=[]
+    for item in xmldoc.getElementsByTagName('artist'):
+        artist = item.getElementsByTagName('name')[0].firstChild
+        if artist is not None:
+            artistlist.append(artist.nodeValue)
+
+    # Stop if charts are empty
+    if len(artistlist)==0:
+        print('#'*20)
+        print("Your charts are empty. I can't proceed.")
+        print('#'*20)
+        sys.exit()
+
+    #Create graph with artists as nodes and create edges between similar artists
+    #graph = pydot.Graph('graphname', graph_type='graph')
+    
+    time.sleep(2)
+    
+    for artist in artistlist[:]:
+        
+        getSimilar(artist.encode('utf8').replace(' ','%20').replace('/','%2F'),cache)
+        #graph.add_node(pydot.Node(artist))
+    
+    #edgelist=[]
+    output = open('test.dot','a')
+    #Edges
+    for artist in artistlist[:]:
+        try:
+            data=open(cache + os.sep + 'similar' + os.sep + artist.encode('utf8').replace(' ','%20').replace('/','%2F') + '.xml','rb')
+            xmldoc=minidom.parse(data)
+            data.close()
+        except Exception as err:
+            print('#'*20)
+            print("Error while parsing your profile. Your username might be "
+              "misspelt or your charts empty.")
+            print('#'*20)
+            sys.exit()
+            
+        simlist=[]
+        
+        for item in xmldoc.getElementsByTagName('artist'):
+            sim = item.getElementsByTagName('name')[0].firstChild
+            if sim is not None:
+                simlist.append(sim.nodeValue)
+        
+        
+        for sim in simlist[:]:
+            if sim in artistlist:
+                t = artist, sim
+                output.write(artist.encode('utf8').replace('/','').replace('\'','').replace(' ','').replace('?','').replace('!','') + ' -> ' + sim.encode('utf8').replace('/','').replace('\'','').replace(' ','').replace('?','').replace('!','') + '\n')
+                #edgelist.append(t)
+            
+    #output = pydot.Dot(pydot.graph_from_edges(edgelist))
+    #output.write('out.dot', prog='none', format='raw')
+
+        
     
     
     
-def download(url,filename)
+    
+    
+    
+def getSimilar(artist,cache):
+    filename = (cache + os.sep + 'similar' + os.sep + artist + '.xml')
+    if not os.path.exists(filename):
+        url = ('http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=' + artist + '&api_key=' + api_key)
+        #url = quote(url)
+        download(url, filename)
+        time.sleep(1)
+    
+def download(url,filename):
+    """ download the binary file at url """
+    print("Downloading:  " + url)
+    instream=urlopen(url)
+    outfile=open(filename,'wb')
+    for chunk in instream:
+        outfile.write(chunk)
+    instream.close()
+    outfile.close()
+    
+    
+########################
+## main
+########################
+def main():
+    makeGraph('alecksphillips',20,'cache','yes')
+
+if __name__=="__main__":
+    main()
